47c8a17250c7ad431a850bfcc1e39b7dfef52ab9
etna-scorpio <etna.scorpio@gmail.com>
2015-07-22 13:39:10 +0300

    done

--- js/common.js
+++ js/common.js
@@ -7,4 +7,29 @@ head.ready(function() {
 		$(this).toggleClass('is-active');
 	});
 
+	// if($('.js-fixed').length > 0){
+
+	// 	var wrapperTop = $('.js-fixed').offset().top;
+	// 	var wrapperHeight = $('.js-fixed').height();
+
+	// 	$(window).scroll(function(){
+	// 		scrollHeader();
+	// 	});
+	// }
+
+	// //fixed header
+	// function scrollHeader(){
+
+	// 	var width = $(window).width(),
+	// 		scrollTop = $(window).scrollTop(),
+	// 		fixed = $('.js-fixed');
+
+	// 	if (scrollTop >= wrapperTop + wrapperHeight + 100) {
+	// 	    fixed.addClass("is-fixed");
+	// 	} else {
+	// 	    fixed.removeClass("is-fixed");
+	// 	}
+	// }
+	// scrollHeader();
+
 });
