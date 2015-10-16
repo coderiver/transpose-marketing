a7430da2c876ac1b07185bd604aa73fdbfcfef99
etna-scorpio <etna.scorpio@gmail.com>
2015-10-16 16:53:13 +0300

    done

--- js/common.js
+++ js/common.js
@@ -1,10 +1,25 @@
 head.ready(function() {
-
-	$('.js-toggle-menu').click(function (e) {
+	$('.js-open-menu').click(function(e) {
 		e.preventDefault();
-
-		$(this).closest('.js-parent').find('.js-menu').toggleClass('is-active');
-		$(this).toggleClass('is-active');
+		$(this).parent().find('.js-menu').addClass('is-active');
 	});
+
+	$('.js-close').click(function(e) {
+		$(this).parent().removeClass('is-active');
+	});
+
+	$(window).scroll(function() {
+		fixedMenu();
+	});
+
+	function fixedMenu() {
+		if ($(window).scrollTop() > 0) {
+			$('.js-fix').addClass('is-fixed');
+		} else {
+			$('.js-fix').removeClass('is-fixed');
+		}
+	}
+
+	fixedMenu();
 
 });
